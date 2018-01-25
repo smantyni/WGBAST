5a6165831860da3a0ed194c185242c869e853d65
hennip <jeannie.jne@gmail.com>
2018-01-25 14:33:55 +0200

    HR graphs ready for BUGS (JAGS part missing)

--- compare-models/compare-HR.R
+++ compare-models/compare-HR.R
@@ -27,10 +27,29 @@ for(y in 1:(length(Years)-1)){
 
 HdcW<-array(NA, dim=c(5,length(Years)-1, 1000))
 HdcR<-array(NA, dim=c(5,length(Years)-1, 1000))
+HdoW<-array(NA, dim=c(5,length(Years)-1, 1000))
+HdoR<-array(NA, dim=c(5,length(Years)-1, 1000))
+HlW<-array(NA, dim=c(5,length(Years)-1, 1000))
+HlR<-array(NA, dim=c(5,length(Years)-1, 1000))
 for(y in 1:(length(Years)-1)){
   for(a in 1:5){
     HdcW[a,y,]<-read.table(paste(sep="", folder1,"/HdcW[",y,",",a,"]1.txt"))[,2]
     HdcR[a,y,]<-read.table(paste(sep="", folder1,"/HdcR[",y,",",a,"]1.txt"))[,2]
+    HdoW[a,y,]<-read.table(paste(sep="", folder1,"/HdoW[",y,",",a,"]1.txt"))[,2]
+    HdoR[a,y,]<-read.table(paste(sep="", folder1,"/HdoR[",y,",",a,"]1.txt"))[,2]
+    HlW[a,y,]<-read.table(paste(sep="", folder1,"/HlW[",y,",",a,"]1.txt"))[,2]
+    HlR[a,y,]<-read.table(paste(sep="", folder1,"/HlR[",y,",",a,"]1.txt"))[,2]
+  }
+}
+
+HoffsW<-array(NA, dim=c(5,(length(Years)-1),1000))
+HoffsR<-array(NA, dim=c(5,(length(Years)-1),1000))
+for(a in 1:5){
+  for(y in 1:(length(Years)-1)){
+    for(s in 1:1000){
+      HoffsW[a,y,s]<-(1-((1-HdoW[a,y,s])*(1-HlW[a,y,s])))
+      HoffsR[a,y,s]<-(1-((1-HdoR[a,y,s])*(1-HlR[a,y,s])))
+    }
   }
 }
 
@@ -89,9 +108,9 @@ for(a in 1:5){
     mutate(age=a, Fishery="CDN", Type="Wild")
   ifelse(a>1, dfW2<-bind_rows(dfW2,dfW),dfW2<-dfW)
   
-  #dfR<-boxplot.bugs.df2(HdcR, a ,1:(length(Years)-1))%>%
-  #  mutate(age=a, Fishery="CDN", Type="Reared")
-  #ifelse(a>1, dfR2<-bind_rows(dfR2,dfR),dfR2<-dfR)
+  dfR<-boxplot.bugs.df2(HdcR, a ,1:(length(Years)-1))%>%
+    mutate(age=a, Fishery="CDN", Type="Reared")
+  ifelse(a>1, dfR2<-bind_rows(dfR2,dfR),dfR2<-dfR)
 }
 
 df<-full_join(dfW2,dfR2, by=NULL)
@@ -104,6 +123,52 @@ df.bugs.Hdc<-as.tibble(setNames(df,c("Year","q5","q25","q50","q75","q95","Age","
                         "2SW"= "2","3SW"= "3",
                         "4SW"= "4","5SW"= "5"))
 df.bugs.Hdc
+
+# OFFSHORE DRIFTNET
+for(a in 1:5){
+  dfW<-boxplot.bugs.df2(HdoW, a ,1:(length(Years)-1))%>%
+    mutate(age=a, Fishery="ODN", Type="Wild")
+  ifelse(a>1, dfW2<-bind_rows(dfW2,dfW),dfW2<-dfW)
+  
+  dfR<-boxplot.bugs.df2(HdoR, a ,1:(length(Years)-1))%>%
+    mutate(age=a, Fishery="ODN", Type="Reared")
+  ifelse(a>1, dfR2<-bind_rows(dfR2,dfR),dfR2<-dfR)
+}
+
+df<-full_join(dfW2,dfR2, by=NULL)
+
+df.bugs.Hdo<-as.tibble(setNames(df,c("Year","q5","q25","q50","q75","q95","Age","Fishery","Type")))%>%
+  select(Age, Fishery, Type, everything())%>%
+  mutate(Year=Year+1986)%>%
+  mutate(Age=fct_recode(factor(Age),
+                        "1SW"="1",
+                        "2SW"= "2","3SW"= "3",
+                        "4SW"= "4","5SW"= "5"))
+df.bugs.Hdo
+
+# OFFSHORE LONGLINE
+for(a in 1:5){
+  dfW<-boxplot.bugs.df2(HlW, a ,1:(length(Years)-1))%>%
+    mutate(age=a, Fishery="OLL", Type="Wild")
+  ifelse(a>1, dfW2<-bind_rows(dfW2,dfW),dfW2<-dfW)
+  
+  dfR<-boxplot.bugs.df2(HlR, a ,1:(length(Years)-1))%>%
+    mutate(age=a, Fishery="OLL", Type="Reared")
+  ifelse(a>1, dfR2<-bind_rows(dfR2,dfR),dfR2<-dfR)
+}
+
+df<-full_join(dfW2,dfR2, by=NULL)
+
+df.bugs.Hl<-as.tibble(setNames(df,c("Year","q5","q25","q50","q75","q95","Age","Fishery","Type")))%>%
+  select(Age, Fishery, Type, everything())%>%
+  mutate(Year=Year+1986)%>%
+  mutate(Age=fct_recode(factor(Age),
+                        "1SW"="1",
+                        "2SW"= "2","3SW"= "3",
+                        "4SW"= "4","5SW"= "5"))
+df.bugs.Hl
+
+# OFFSHORE COMBINED
 
 
 # Model 2: JAGS
@@ -135,26 +200,110 @@ df.bugs.Hdc
 
 ## ---- graphs-HR
 
-# Wild
-df1<-filter(df.bugs, Type=="Wild")
-df2<-filter(df.jags, Type=="Wild")
+# Hdo
+df1<-filter(df.bugs.Hdo, Type=="Wild")
+#df2<-filter(df.jags, Type=="Wild")
 
-ggplot(df2, aes(Year))+
+ggplot(df1, aes(Year))+
+  theme_bw()+
+  geom_boxplot(
+    mapping= aes(ymin = q5, lower = q25, middle = q50, upper = q75, ymax = q95),
+    stat = "identity",
+    colour="grey", fill="grey95")+
+#  geom_boxplot(
+#    data=df2,
+#    aes(ymin = q5, lower = q25, middle = q50, upper = q75, ymax = q95),
+#    stat = "identity",fill=rgb(1,1,1,0.6))+
+#  geom_line(data=df2,aes(Year,q50))+
+  labs(x="Year", y="Harvest rate", title="Offshore driftnet, wild")+
+  geom_line(aes(Year,q50),col="grey")+
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 5))+
+  facet_wrap(~Age)
+
+# Hl
+df1<-filter(df.bugs.Hl, Type=="Wild")
+#df2<-filter(df.jags, Type=="Wild")
+
+ggplot(df1, aes(Year))+
   theme_bw()+
   geom_boxplot(
     data=df1,
     mapping= aes(ymin = q5, lower = q25, middle = q50, upper = q75, ymax = q95),
     stat = "identity",
     colour="grey", fill="grey95")+
-  geom_boxplot(
-    aes(ymin = q5, lower = q25, middle = q50, upper = q75, ymax = q95),
-    stat = "identity",fill=rgb(1,1,1,0.6))+
-  labs(x="Year", y="Proportion per age group", title="Homing rates, wild")+
-  geom_line(aes(Year,q50))+
-  geom_line(data=df1,aes(Year,q50),col="grey")+
+  #  geom_boxplot(
+  #    data=df2,
+  #    aes(ymin = q5, lower = q25, middle = q50, upper = q75, ymax = q95),
+  #    stat = "identity",fill=rgb(1,1,1,0.6))+
+  #geom_line(data=df2,aes(Year,q50))+
+  labs(x="Year", y="Harvest rate", title="Offshore longline, wild")+
+  geom_line(aes(Year,q50),col="grey")+
   scale_x_continuous(breaks = scales::pretty_breaks(n = 5))+
   facet_wrap(~Age)
 
 
+# coastal TN & GN
+df1<-filter(df.bugs.Hc, Type=="Wild")
+#df1<-filter(df.bugs.Hc, Type=="Wild", AU==1)
+#df2<-filter(df.jags, Type=="Wild")
+
+ggplot(df1, aes(Year))+
+  theme_bw()+
+  geom_boxplot(
+    mapping= aes(ymin = q5, lower = q25, middle = q50, upper = q75, ymax = q95),
+    stat = "identity",
+    colour="grey", fill="grey95")+
+  #  geom_boxplot(
+  #    data=df2,
+  #    aes(ymin = q5, lower = q25, middle = q50, upper = q75, ymax = q95),
+  #    stat = "identity",fill=rgb(1,1,1,0.6))+
+  #geom_line(data=df2,aes(Year,q50))+
+  labs(x="Year", y="Harvest rate", title="Coastal TN & GN, wild AU1")+
+  geom_line(aes(Year,q50),col="grey")+
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 5))+
+#  facet_wrap(~Age)
+  facet_grid(AU~Age)
+
+
+# coastal DN
+df1<-filter(df.bugs.Hdc, Type=="Wild")
+#df2<-filter(df.jags, Type=="Wild")
+
+ggplot(df1, aes(Year))+
+  theme_bw()+
+  geom_boxplot(
+    mapping= aes(ymin = q5, lower = q25, middle = q50, upper = q75, ymax = q95),
+    stat = "identity",
+    colour="grey", fill="grey95")+
+  #  geom_boxplot(
+  #    data=df2,
+  #    aes(ymin = q5, lower = q25, middle = q50, upper = q75, ymax = q95),
+  #    stat = "identity",fill=rgb(1,1,1,0.6))+
+  #geom_line(data=df2,aes(Year,q50))+
+  labs(x="Year", y="Harvest rate", title="Coastal DN, wild")+
+  geom_line(aes(Year,q50),col="grey")+
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 5))+
+  facet_wrap(~Age)
+
+
+# river
+df1<-filter(df.bugs.Hr, Type=="Wild")
+#df2<-filter(df.jags, Type=="Wild")
+
+ggplot(df1, aes(Year))+
+  theme_bw()+
+  geom_boxplot(
+    mapping= aes(ymin = q5, lower = q25, middle = q50, upper = q75, ymax = q95),
+    stat = "identity",
+    colour="grey", fill="grey95")+
+  #  geom_boxplot(
+  #    data=df2,
+  #    aes(ymin = q5, lower = q25, middle = q50, upper = q75, ymax = q95),
+  #    stat = "identity",fill=rgb(1,1,1,0.6))+
+  #geom_line(data=df2,aes(Year,q50))+
+  labs(x="Year", y="Harvest rate", title="River, wild")+
+  geom_line(aes(Year,q50),col="grey")+
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 5))+
+  facet_wrap(~Age)
 
 
