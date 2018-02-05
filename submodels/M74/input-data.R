87e9a453ab35f25c500ea8161686d1aee187962e
hennip <jeannie.jne@gmail.com>
2018-02-05 11:11:42 +0200

    wrangle M74 data

--- submodels/M74/input-data.R
+++ submodels/M74/input-data.R
@@ -11,37 +11,58 @@ library(coda)
 pathM74<-"H:/Biom/SubE_M74/2018/"
 
 
-ColNames<-c("smolts", "empty", "n_schools", "school_size")
-Day<-c(c(1:30), c(1:31), c(1:31))
-Month<-c(rep(6,30), rep(7,31), rep(8,31))
-
+# FI data
 df<-read_xlsx(str_c(pathM74,"data/orig/Finnish_M74_data-2017_paivitetty.xlsx"), 
-              col_names = T, guess_max = 10000, sheet=1, na="")
-
-df<-df%>%   mutate(River=fct_recode(RIVER,
+              col_names = T, guess_max = 10000, sheet=1, na="")%>%   
+  mutate(river=fct_recode(RIVER,
                                     "1"="Simo",
                                     "2"="Tornio",
                                     "3"="Kemi",
                                     "4"="Iijoki"))%>%
-  mutate(year=FEMALE_YEAR-1984)
+  mutate(YEAR=FEMALE_YEAR)%>%
+  mutate(year=FEMALE_YEAR-1984)%>%
+  mutate(Eggs=ifelse(is.na(eggs)==F,eggs,ifelse(year<10,100,115)))%>%
+  mutate(eggs=Eggs)%>%
+  mutate(surv_eggs=round(eggs*(1-(YSFM/100)),0) )%>%
+  mutate(M74_mort=fct_recode(M74,
+                             "1"="Ei",
+                             "2"="M74"))%>%
+  # mort100 is 2 if 100% mortality, 1 if <100% and NA if M74 unknown (XX should not appear anywhere)
+  mutate(mortality100=ifelse(is.na(M74)==F, ifelse(is.na(M74_100)==F, ifelse(M74_100==100,2,"XX"),1),NA ))
   
-  df<-df%>%mutate(Eggs=ifelse(is.na(eggs)==F,eggs,ifelse(year<10,100,115)))
-filter(df, is.na(Eggs)==T)
+# check that these match                      
+#View(select(df, M74,`M74 mortality`,  M74_mort,M74_100,`100% mortality`, mortality100))
+#View(select(df, `surviving eggs`, surv_eggs))
+            
+
+dfFI<-df%>% 
+  select(YEAR, eggs, year, river, surv_eggs, M74_mort, mortality100, YSFM)%>%
+  mutate(M74=M74_mort)%>%
+  select(YEAR, eggs, year, river, surv_eggs, M74, mortality100, YSFM)%>%
+  filter(river!=4) # remove iijoki for now. Avoid confusion with Luleälven.
+  
+#View(filter(df, river==4))
 
 
-View(df)
+# SE data
+df1<-read_tsv(str_c(pathM74,"data/der/datR_M74_SE16.txt"))
 
-read_xl
-             
-D05<-read_xls(str_c(sep="",pathIn2,"UTSJOKI VIDEODATA/Utsjoki_smoltit 2005.xls"),
-              sheet=1, na="", 
-              range="Z10:AC70", col_names=ColNames)
-D05[23,]<-rep(NA,4) # 23.6. 00-09 missing
-tmp<-array(0, dim=c(31,4));colnames(tmp)<-colnames(D05)
-D05<-rbind(D05, tmp) #1.8.-31.8. missing but replace with zeros
-D05<-D05 %>% mutate(day=c(1:92))%>%
-  mutate(Year=2005)%>%
-  mutate(Day=Day)%>%
-  mutate(Month=Month)%>%
-  select(Year,Month,Day,day,smolts, school_size)
-#View(D05)
+df2<-read_xlsx(str_c(pathM74,"data/der/Swedish_M74_data_17.xlsx"))%>%
+  mutate(ss=fct_recode(river,
+                       "11"="Dal", "10"="Ljusnan", "8"="Indals", "7"="Angerman",
+                       "6"="Ume", "5"="Skellefte", "4"="Lule"
+                       ))%>%
+  mutate(yy=32)%>%
+  mutate(ff=Kramade)%>%
+  mutate(xx=`Antal M74`)%>%
+  select(ss, yy, ff, xx)
+
+  
+
+
+df2
+View(df1)
+
+dfSE
+
+
