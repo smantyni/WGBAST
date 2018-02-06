97e5f89d64603ee8b402e84989abe02c6c5d22cb
hennip <jeannie.jne@gmail.com>
2018-02-06 13:18:12 +0200

    data ready

---
+++ submodels/M74/data-M74.R
@@ -0,0 +1,151 @@
+library(rjags)
+library(runjags)
+library(tidyverse)
+library(readxl)
+library(forcats)
+library(lubridate)
+library(stringr)
+library(gridExtra)
+library(coda)
+
+pathM74<-"H:/Biom/SubE_M74/2018/"
+
+
+# FI data
+df<-read_xlsx(str_c(pathM74,"data/orig/Finnish_M74_data-2017_paivitetty.xlsx"), 
+              col_names = T, guess_max = 10000, sheet=1, na="")%>%   
+  mutate(river=fct_recode(RIVER,
+                                    "1"="Simo",
+                                    "2"="Tornio",
+                                    "3"="Kemi",
+                                    "4"="Iijoki"))%>%
+  mutate(YEAR=FEMALE_YEAR)%>%
+  mutate(year=FEMALE_YEAR-1984)%>%
+  mutate(Eggs=ifelse(is.na(eggs)==F,eggs,ifelse(year<10,100,115)))%>%
+  mutate(eggs=Eggs)%>%
+  mutate(surv_eggs=round(eggs*(1-(YSFM/100)),0) )%>%
+  mutate(M74_mort=fct_recode(M74,
+                             "1"="Ei",
+                             "2"="M74"))%>%
+  # mort100 is 2 if 100% mortality, 1 if <100% and NA if M74 unknown (XX should not appear anywhere)
+  mutate(mortality100=ifelse(is.na(M74)==F, ifelse(is.na(M74_100)==F, ifelse(M74_100==100,2,"XX"),1),NA ))
+
+# check that these match                      
+#View(select(df, M74,`M74 mortality`,  M74_mort,M74_100,`100% mortality`, mortality100))
+#View(select(df, `surviving eggs`, surv_eggs))
+            
+
+dfFI<-df%>% 
+  mutate(M74=parse_double(M74_mort))%>%
+  select(YEAR, year, RIVER, river, eggs, surv_eggs, M74, mortality100, YSFM)
+#%>%filter(river!=4) # to remove iijoki, avoiding confusion with Luleälven.
+  
+#View(filter(df, river==4))
+
+
+# SE data
+df1<-read_tsv(str_c(pathM74,"data/der/datR_M74_SE16.txt"), 
+              col_types = cols(yy="i",ss="c",ff="i", xx="i"))%>%
+  mutate(s=parse_double(ss))
+
+
+df2<-read_xlsx(str_c(pathM74,"data/der/Swedish_M74_data_17.xlsx"))%>%
+  mutate(ss=fct_recode(river,
+                       "11"="Dal", "10"="Ljusnan", "8"="Indals", "7"="Angerman",
+                       "6"="Ume", "5"="Skellefte", "4"="Lule"
+                       ))%>%
+  mutate(yy=32)%>%
+  mutate(ff=Kramade)%>%
+  mutate(xx=`Antal M74`)%>%
+  select(ss, yy, ff, xx)%>%
+  mutate(s=parse_double(ss))
+
+# add missing data for 
+df3<-tibble(
+  yy=32,
+  s=c(9,12,13),
+  ff=100,
+  xx=parse_double(NA)
+)
+  
+dfSE<-full_join(df1,df2)
+dfSE<-full_join(dfSE,df3)
+
+dfSE<-dfSE%>%
+  mutate(stock=s+1)%>% # +1 is needed to start SE stocks from index 5 (Iijoki included as stock 4)
+  select(yy,stock,ff,xx)
+
+#View(dfFI)
+#View(dfSE)
+
+
+# input to BUGS:
+
+length(dfFI$eggs)
+# 1437
+length(dfSE$xx)
+# 320
+
+dfFI.bugs<-dfFI%>%
+  select(eggs, year, river, surv_eggs, M74, mortality100)
+
+dfSE.bugs<-dfSE
+  
+write_tsv(dfFI.bugs, str_c(pathM74,"prg/input/M74dataFI17.txt"))
+write_tsv(dfSE.bugs, str_c(pathM74,"prg/input/M74dataSE17.txt"))
+
+
+# wrangle for figures
+
+by_ry<-dfFI%>%
+  group_by(RIVER, YEAR)
+  
+ysfm<-by_ry%>%
+  summarise(ysfm=round(mean(YSFM/100),2),
+            N_fem=n())
+
+n_M74notNA<-dfFI%>%
+  filter(is.na(M74)==F)%>%
+  group_by(RIVER, YEAR)%>%
+  summarise(N_M74=n())
+
+cases_M74<-by_ry%>% 
+  count(M74)%>%
+  filter(M74==2)%>%
+  mutate(N_M74fem=n)
+
+cases_mort100<-by_ry%>% 
+  count(mortality100)%>%
+  filter(mortality100==2)%>%
+  mutate(N_mort100=n)
+
+tmp<-full_join(ysfm, n_M74notNA, by=c("RIVER", "YEAR"))
+tmp<-full_join(tmp, cases_M74, by=c("RIVER", "YEAR"))
+dfFI.2<-full_join(tmp, cases_mort100, by=c("RIVER", "YEAR"))%>%
+  select(RIVER, YEAR, ysfm, N_fem, N_M74, N_M74fem, N_mort100)%>%
+  mutate(N_M74fem=ifelse(is.na(N_M74fem)==T, ifelse(is.na(N_M74)==T, NA, 0),N_M74fem))%>%
+  mutate(N_mort100=ifelse(is.na(N_mort100)==T, ifelse(is.na(N_M74)==T, NA, 0),N_mort100))%>%
+  mutate(propM74=round(N_M74fem/N_fem,2))%>%
+  mutate(prop_mort100=round(N_mort100/N_M74,2))
+
+           
+View(df)
+
+dfSE.2<-dfSE%>%
+  mutate(propM74=round(xx/ff,2))%>%
+  mutate(RIVER=fct_recode(parse_character(stock),
+                          "Lule"="5","Skellefte"="6",
+                          "Ume"="7","Angerman"="8",
+                          "Indals"="9","Ljungan"="10",
+                          "Ljusnan"="11","Dal"="12",
+                          "Morrum"="13","Unknown"="14"))%>%
+  mutate(YEAR=yy+1984)%>%
+  mutate(N_fem=ff)%>%
+  mutate(N_M74fem=xx)%>%
+  select(YEAR, RIVER, N_fem, N_M74fem, propM74)
+
+View(tmp)
+
+tmp<-full_join(dfFI.2,dfSE.2)%>%
+  select(-N_M74)
+
--- submodels/M74/model-M74.R
+++ submodels/M74/model-M74.R
@@ -1,68 +1,65 @@
-
+# For reason or another this does not run in JAGS
+# -> use BUGS for now.
 
 
 
 M1<-"
-
 model{
+  for(i in 1:N_FI){ #number of females in the Finnish data
+    x[i]~dbin(p[i,j[i]], Eggs[i]) # likelihood function: x = number of surviving eggs, p= probability of survival, Egg = total number of eggs
 
-for (i in 1:n){ #number of females in the Finnish data
-x[i] ~dbin(p[i,j[i]], Eggs[i]) # likelihood function: x = number of surviving eggs, p= probability of survival, Egg = total number of eggs
+    j[i]~dcat(q[year[i],ss[i], 1:2]) # index if the female has M74 or not
+    p[i,1]<- (1-M_YSFM[i])  # survival from normal yolk-sac-fry mortality
+    p[i,2] <- (1-M_YSFM[i])*(1-M74[year[i],i,k[i]]) # survival from normal and M74 mortality
 
-j[i]~dcat(q[year[i],ss[i], ]) # index if the female has M74 or not
-p[i,1]<- (1-M_YSFM[i])  # survival from normal yolk-sac-fry mortality
-p[i,2] <- (1-M_YSFM[i])*(1-M74[year[i],i,k[i]]) # survival from normal and M74 mortality
+    k[i]~dcat(qq[year[i],ss[i],1:2]) # index if the female has 100% M74 mortality or not
+    M74[year[i],i,1] ~dbeta(a_M74[year[i]],b_M74[year[i]]) # M74 mortality
+    M74[year[i],i,2] <- 1 # 100% M74 mortality
+    M_YSFM[i]~dbeta(a_M_YSFM,b_M_YSFM) # normal yolk-sac-fry mortality
+  }
 
-k[i]~dcat(qq[year[i],ss[i],]) # index if the female has 100% M74 mortality or not
-M74[year[i],i,1] ~dbeta(a_M74[year[i]],b_M74[year[i]]) # M74 mortality
-M74[year[i],i,2] <- 1 # 100% M74 mortality
-M_YSFM[i]~dbeta(a_M_YSFM,b_M_YSFM) # normal yolk-sac-fry mortality
+  # transformation from mean and eta into beta-parameters
+  a_M_YSFM<- mu_M_YSFM * eta_M_YSFM
+  b_M_YSFM<- (1- mu_M_YSFM) * eta_M_YSFM
+  mu_M_YSFM~dbeta(2,2)I(0.01,0.99) # mean normal yolk-sac-fry mortality
+  eta_M_YSFM~dunif(2,1000) # indicator of variance in normal yolk-sac-fry mortality
 
+  for(y in 1:32){ # Years , last spawning yearclass in the data is 30=2014
+    a_M74[y] <- mu_M74[y] * eta_M74[y] # conversion of mean and variance into alpha parameter of beta distribution
+    b_M74[y] <- (1- mu_M74[y]) * eta_M74[y] # conversion of mean and variance into beta parameter of beta distribution
+    mu_M74[y]~dbeta(2,2)I(0.01,0.99) # mean M74 value for each year
+    eta_M74[y]~dunif(2,1000) # indicator of variance between stocks
+
+    for(s in 1:14){  # Obs! Kemijoki added
+      surv2_M74[y,s] <-  (qq[y,s,1] * (1-mu_M74[y])) + (qq[y,s,2]) *0 # probability of offspring surviving M74 mortality if female has M74
+      mean2_M74[y,s] <- 1 - surv2_M74[y,s] #probability of offspring dying because of M74 mortality if female has M74
+
+      surv_M74[y,s] <- (q[y,s,1] *1)+ (q[y,s,2] * qq[y,s,1] * (1-mu_M74[y])) + (q[y,s,2] * qq[y,s,2] *0) # probability of offspring surviving M74 mortality
+      mean_M74[y,s] <- 1 - surv_M74[y,s] #probability of offspring dying because of M74 mortality
+      
+      q[y,s,1] ~dbeta(a_q[y],b_q[y]) # probability of a female not having M74 syndromes
+      q[y,s,2] <- 1- q[y,s,1] # probability of a female having M74 syndromes
+      
+      qq[y,s,1] ~dbeta(a_qq[y],b_qq[y]) # probability of a female not having 100 % M74 mortality
+      qq[y,s,2] <- 1- qq[y,s,1] # probability of a female having 100 % M74 mortality
+    }
+
+    a_q[y] <- mu_q[y] * eta_q
+    b_q[y] <- (1- mu_q[y]) * eta_q
+    mu_q[y]~dbeta(2,2)I(0.01,0.99)# mean probability of a female not having M74 syndromes
+    
+    a_qq[y] <- mu_qq[y] * eta_qq
+    b_qq[y] <- (1- mu_qq[y]) * eta_qq
+    mu_qq[y]~dbeta(2,2)I(0.05,0.95)# mean probability of a female not having 100 % M74 mortality
+  }
+
+  eta_q~dunif(2, 1000)# indicator of variance in q
+  eta_qq~dunif(10, 1000)# indicator of variance in qq
+
+  for (i in 1:N_SE){ # number of rows in the Swedish data 
+    xx[i]~dbin(q[yy[i], stock[i], 2], Females[i]) # likelihood function: xx = number of females affected by M74, p= probability of a female having M74, females = total number of females
+  }
 }
-# transformation from mean and eta into beta-parameters
-a_M_YSFM<- mu_M_YSFM * eta_M_YSFM
-b_M_YSFM<- (1- mu_M_YSFM) * eta_M_YSFM
-mu_M_YSFM~dbeta(2,2)I(0.01,0.99) # mean normal yolk-sac-fry mortality
-eta_M_YSFM~dunif(2,1000) # indicator of variance in normal yolk-sac-fry mortality
-
-for (y in 1:31){ # Years , last spawning yearclass in the data is 30=2014
-a_M74[y] <- mu_M74[y] * eta_M74[y] # conversion of mean and variance into alpha parameter of beta distribution
-b_M74[y] <- (1- mu_M74[y]) * eta_M74[y] # conversion of mean and variance into beta parameter of beta distribution
-mu_M74[y] ~dbeta(2,2)I(0.01,0.99) # mean M74 value for each year
-eta_M74[y]~dunif(2,1000) # indicator of variance between stocks
-
-for (s in 1:13){  # Obs! Kemijoki added
-
-surv2_M74[y,s] <-  (qq[y,s,1] * (1-mu_M74[y])) + (qq[y,s,2]) *0 # probability of offspring surviving M74 mortality if female has M74
-mean2_M74[y,s] <- 1 - surv2_M74[y,s] #probability of offspring dying because of M74 mortality if female has M74
-
-surv_M74[y,s] <- (q[y,s,1] *1)+ (q[y,s,2] * qq[y,s,1] * (1-mu_M74[y])) + (q[y,s,2] * qq[y,s,2] *0) # probability of offspring surviving M74 mortality
-mean_M74[y,s] <- 1 - surv_M74[y,s] #probability of offspring dying because of M74 mortality
-
-q[y,s,1] ~dbeta(a_q[y],b_q[y]) # probability of a female not having M74 syndromes
-q[y,s,2] <- 1- q[y,s,1] # probability of a female having M74 syndromes
-
-qq[y,s,1] ~dbeta(a_qq[y],b_qq[y]) # probability of a female not having 100 % M74 mortality
-qq[y,s,2] <- 1- qq[y,s,1] # probability of a female having 100 % M74 mortality
-
-}
-
-a_q[y] <- mu_q[y] * eta_q
-b_q[y] <- (1- mu_q[y]) * eta_q
-mu_q[y]~dbeta(2,2)I(0.01,0.99)# mean probability of a female not having M74 syndromes
-
-a_qq[y] <- mu_qq[y] * eta_qq
-b_qq[y] <- (1- mu_qq[y]) * eta_qq
-mu_qq[y]~dbeta(2,2)I(0.05,0.95)# mean probability of a female not having 100 % M74 mortality
-
-}
-eta_q~dunif(2, 1000)# indicator of variance in q
-eta_qq~dunif(10, 1000)# indicator of variance in qq
-
-
-for (i in 1:310){ # number of rows in the Swedish data 
-xx[i] ~dbin(q[yy[i], stock[i], 2], Females[i]) # likelihood function: xx = number of females affected by M74, p= probability of a female having M74, females = total number of females
-}}
 "
 
 
@@ -73,33 +70,24 @@ cat(M1,file=Mname)
 
 data<-list(
   # FI data:
-  Eggs= df$eggs,
-  year= df$year,
-  ss=df$river,
-  x=surv_eggs,
-  j=M74,
-  k=mortality100,
-  n=length(df$eggs), # number of females/rows
+  Eggs= dfFI$eggs,
+  year= dfFI$year,
+  ss=dfFI$river,
+  x=dfFI$surv_eggs,
+  j=dfFI$M74,
+  k=dfFI$mortality100,
+  N_FI=length(dfFI$eggs), # number of females/rows
 # SE data:
-    )
-
-
-
-
-inits<-list(list(LNtot=rep(14,data$nYears),zN=array(1, dim=c(61,data$nYears))),
-            list(LNtot=rep(14,data$nYears),zN=array(1, dim=c(61,data$nYears))))
+  yy=dfSE$yy,
+  stock=dfSE$stock,
+  Females=dfSE$ff,
+  xx=dfSE$xx,
+  N_SE=length(dfSE$xx) # number of rows
+)
 
 
 var_names<-c(
-  "aD","bD","cvD","cvmuD",
-  #  "K","slope","cvS", "cvmuS",
-  "sums1","sums2",
-  "aP","bP","sdP",
-  #  "etaB",
-  "aB","bB","sdBB",
-  "eta_alphaN",
-  #"Nobs",  
-  "Ntot","N"
+  "mean_M74", "mean2_M74"
 )
 
 
@@ -108,7 +96,7 @@ var_names<-c(
 
 t1<-Sys.time()
 run1 <- run.jags(M1, 
-                 monitor= var_names,data=data,inits = inits,
+                 monitor= var_names,data=data,#inits = inits,
                  n.chains = 2, method = 'rjparallel', thin=300, burnin =0, 
                  modules = "mix",keep.jags.files=F,sample =1000, adapt = 100, 
                  progress.bar=TRUE)
@@ -116,84 +104,3 @@ t2<-Sys.time()
 difftime(t2,t1)
 # 17h
 
-run<-run1
-save(run, file=str_c(pathOut,modelName,"_",dataName,"_run.RData"))
-
-t1<-Sys.time()
-run2 <- extend.jags(run1, combine=F, sample=2000, thin=300, keep.jags.files=F)
-t2<-Sys.time()
-difftime(t2,t1)
-#1.1d
-
-run<-run2
-save(run, file=str_c(pathOut,modelName,"_",dataName,"_run.RData"))
-
-t1<-Sys.time()
-run3 <- extend.jags(run2, combine=T, sample=2000, thin=300, keep.jags.files=F)
-t2<-Sys.time()
-difftime(t2,t1)
-#1.1d
-
-run<-run3
-save(run, file=str_c(pathOut,modelName,"_",dataName,"_run.RData"))
-
-t1<-Sys.time()
-run4 <- extend.jags(run3, combine=T, sample=3000, thin=300, keep.jags.files=F)
-t2<-Sys.time()
-difftime(t2,t1)
-#1.6d
-run<-run4
-save(run, file=str_c(pathOut,modelName,"_",dataName,"_run.RData"))
-
-t1<-Sys.time()
-run5 <- extend.jags(run4, combine=T, sample=3000, thin=300, keep.jags.files=F)
-t2<-Sys.time()
-difftime(t2,t1)
-#1.6d
-run<-run5
-save(run, file=str_c(pathOut,modelName,"_",dataName,"_run.RData"))
-
-t1<-Sys.time()
-run6 <- extend.jags(run5, combine=T, sample=4000, thin=300, keep.jags.files=F)
-t2<-Sys.time()
-difftime(t2,t1)
-#2.1d
-
-run<-run6
-save(run, file=str_c(pathOut,modelName,"_",dataName,"_run.RData"))
-
-
-run<-run3
-
-summary(run, var="D")
-summary(run, var="P")
-summary(run, var="B")
-summary(run, var="Ntot")
-summary(run, var="eta_alphaN")
-summary(run, var="sum")
-
-
-plot(run, var="D")
-plot(run, var="P")
-plot(run, var="B")
-plot(run, var="Ntot")
-plot(run, var="eta_alphaN")
-#plot(run, var="sum")
-
-chains<-as.mcmc.list(run)
-chains<-window(chains,start=1000000)
-#save(run, file="H:/Projects/ISAMA/prg/output/Utsjoki-smolts/Smolts_etaB_0714_run.RData")
-save(chains, file="H:/Projects/ISAMA/prg/output/Utsjoki-smolts/Smolts_etaB_0714_chains.RData")
-
-
-gelman.diag(chains[,"Ntot[1]"])
-gelman.diag(chains[,"Ntot[2]"])
-gelman.diag(chains[,"Ntot[3]"])
-gelman.diag(chains[,"Ntot[4]"])
-gelman.diag(chains[,"Ntot[5]"])
-
-gelman.diag(chains[,"aP"])
-gelman.diag(chains[,"bP"])
-gelman.diag(chains[,"aD"])
-gelman.diag(chains[,"cvD"])
-gelman.diag(chains[,"cvmuD"])
