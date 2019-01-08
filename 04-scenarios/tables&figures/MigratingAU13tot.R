2cb044687f516615387453e0c4764cf707904fd6
hennip <jeannie.jne@gmail.com>
2019-01-08 13:30:24 +0200

    Calculate reared grilse proportion returning GoB

--- 04-scenarios/tables&figures/MigratingAU13tot.R
+++ 04-scenarios/tables&figures/MigratingAU13tot.R
@@ -31,7 +31,7 @@ Nstocks<-16
 choice<-"MED"  
 
 #! Effort 
-EffScen<-5 
+EffScen<-1
 
 #Load the file containing stats
 File<-paste0(PathScen,"ScenProj_",Model,"_Mps",choice,"_EScen",EffScen,".RData")
@@ -57,4 +57,37 @@ for(i in 1:Nyears){
 
 res<-cbind(year,mu, sd, q5, q50, q95)
 write.xlsx(res,paste0(PathOut,"migratingAU13.xlsx"))
+
+# Reared grilse proportions, AU 1-3
+
+# Fourth index (1) is migrating individuals
+MigrRAU13_Age<-array(NA, dim=c(6,Nyears,1000))
+for(a in 1:6){
+  for(y in 1:Nyears){
+  for(i in 1:1000){
+    MigrRAU13_Age[a,y,i]<-  sum(MigrR[a,y,1:3,1,i], na.rm=T)
+  }
+}
+}
+GrilseProp<-array(NA, dim=c(Nyears, 1000))
+for(y in 1:Nyears){
+  for(i in 1:1000){
+    GrilseProp[y,i]<-MigrRAU13_Age[2,y,i]/sum(MigrRAU13_Age[2:6,y,i])
+    }}
+
+Q5<-c()
+Q50<-c()
+Q95<-c()
+
+for(y in 2:Nyears){
+  tmp<-summary(as.mcmc(GrilseProp[y,]), quantiles=c(0.05,0.5,0.95))
+  Q5[y]<-tmp$quantiles[1]
+  Q50[y]<-tmp$quantiles[2]
+  Q95[y]<-tmp$quantiles[3]
+  }
+
+res<-cbind(c(1992:(1992+Nyears-1)),Q5, Q50, Q95)
+write.xlsx(res,paste0(PathOut,"migratingAU13R_GrilseProp.xlsx"))
+
+
 
